d3b5b98ab20867357ce0a0be08fa06d2917b62ec
Роман Мотрич < >
2017-02-18 14:46:34 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String Str = new String("Добро-пожаловать-на-prog.kiev.ua");
+
+        System.out.println("Возвращаемое значение: ");
+        for (String retval : Str.split("-", 2)) {
+            System.out.println(retval);
+        }
+
+        System.out.println();
+        System.out.println("Возвращаемое значение: ");
+        for (String retval : Str.split("-", 3)) {
+            System.out.println(retval);
+
+        }
+        System.out.println();
+        System.out.println("Возвращаемое значение: ");
+        for (String retval : Str.split("-", 0)) {
+            System.out.println(retval);
+
+
+        }
+        System.out.println();
+        System.out.println("Возвращаемое значение: ");
+        for (String retval : Str.split("-")) {
+            System.out.println(retval);
+
+        }
+    }
+}
+
